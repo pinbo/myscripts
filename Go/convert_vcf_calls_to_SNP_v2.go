6e675e2c7fcc1f935d247d35aed9e0f745b55303
Junli Zhang <zhjl86@gmail.com>
2019-09-18 21:47:13 -0700

    add another version of vcf convert

---
+++ Go/convert_vcf_calls_to_SNP_v2.go
@@ -0,0 +1,78 @@
+// skip checking comments after the header, but did not increase the speed.
+// seems if statement is okay.
+
+package main
+import (
+    "bufio"
+    "fmt"
+	"os"
+	"log"
+)
+import s "strings"
+
+//var p = fmt.Println
+
+func gt2snp (allele_list []string , gt string) string {
+	// suppose unphased calls, that is separated by "/", not by "|"
+	// only GT fields with 4 types: 0/0, 0/1, 1/1, or ./.
+	switch gt {
+	case "0/0":
+		return allele_list[0]
+	case "1/1":
+		return allele_list[1]
+	case "0/1":
+		return "H"
+	case "./.":
+		return "N"
+	}
+	return gt // in case other cases, for example 2/2 when more than 2 alleles
+}
+
+func main () {
+	if len(os.Args) < 3 {
+		fmt.Println("Please provide 2 arguments: input vcf file, output file name")
+		os.Exit(1)
+	}
+	input := os.Args[1]
+	output := os.Args[2]
+	geno_starts := 10 // 10th column starts the genotype information
+
+	infile, err := os.Open(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	outfile, err := os.Create(output)
+	//w := bufio.NewWriter(outfile)
+
+	defer infile.Close()
+	defer outfile.Close()
+
+	scanner := bufio.NewScanner(infile)
+	for scanner.Scan() {
+		//fmt.Println(scanner.Text())
+		line := scanner.Text() // s.TrimSuffix(scanner.Text(), "\n")
+		if s.HasPrefix(line, "#CHROM") {
+			ll := s.Split(line, "\t")
+			outline := s.Join(append(ll[0:5], ll[(geno_starts - 1):]...), "\t")
+			fmt.Fprintln(outfile, outline)
+			break
+		}
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		ll := s.Split(line, "\t")
+		GTs := ll[(geno_starts - 1):] // all GT calls
+		ref := []string{ll[3]}
+		alt := s.Split(ll[4], ",") // there may be more than one alternative alleles
+		alleles := append(ref,  alt...) // this way, 0 will be ref, and 1, 2 ... will be alternative allele
+		SNPs := make([]string, len(GTs))
+		for pos, num := range GTs {
+			SNPs[pos] = gt2snp(alleles, num)
+		}
+		outline := s.Join(append(ll[0:5], SNPs...), "\t")
+		//n4, err := w.WriteString("buffered\n")
+		fmt.Fprintln(outfile, outline)
+	}
+}
