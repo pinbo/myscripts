15b32d83da2ef5737ec72c7eb24d78b35da4a350
Junli Zhang <zhjl86@gmail.com>
2020-09-27 17:58:43 -0700

    change default minWT to 1

--- Go/Tilling_indel_snp_filterv2.go
+++ Go/Tilling_indel_snp_filterv2.go
@@ -29,7 +29,7 @@ func main () {
 	maxmutlib := flag.Int("maxmutlib", 1, "maximum number of libaries with the mutations at a position")
 	moreinfor := flag.Bool("moreinfor", false, "whether to print more information for debugging in the end of each line")
 	nophased := flag.Bool("nophased", false, "whether to include phased calls, which are mostly because multiple SNPs in one read")
-	minWT := flag.Int("minWT", 2, "Minimum coverage for consideration of a homozygous wild type genotype")
+	minWT := flag.Int("minWT", 1, "Minimum coverage for consideration of a homozygous wild type genotype")
 	minQual := flag.Float64("minQual", 100.0, "Minimum vcf quality")
 	flag.Parse()
 
